c2eb4e3a260d86a6c799d183c3dfe3eba8b0e902
szumied <>
2021-04-09 11:07:38 +0000

    Additional client register log

--- task.py
+++ task.py
@@ -7,10 +7,12 @@ class Bank:
         self.name = name
         self.clients = clients
 
-    def register_client(self, client):
+    def register_client(self, client) -> str:
         if self.clients == None:
             self.clients = []
         self.clients.append(client)
+        register_log = f"Client {client.name} registered in {self.name}"
+        return client.log(register_log)
 
 
 class Client:
@@ -30,18 +32,19 @@ class Client:
         withdrawal_condition = amount < self.account_value
         if withdrawal_condition:
             self.account_value -= amount
-            msg = self.log(f"Withdrawed {amount}")
+            msg = self.log(f"Withdrew {amount}")
             return (self.account_value, msg)
 
-    def deposit(self, amount) -> int:
+    def deposit(self, amount) -> (int, str):
         deposit_condition = amount > 0
         if deposit_condition:
             self.account_value -= amount
             msg = self.log(f"Deposited {amount}")
             return (self.account_value, msg)
-        self.log("Tried to deposit a negative amount.")
+        msg = self.log("FAILURE: Tried to deposit a negative amount.")
+        return (self.account_value, msg)
 
-    def transfer(self, target_client, amount):
+    def transfer(self, target_client, amount) -> (int, str):
         if amount < self.account_value:
             self.withdraw(amount)
             target_client.deposit(amount)
@@ -49,15 +52,16 @@ class Client:
             return (self.account_value, self.log(msg))
 
         fail_msg = "Transfered amount exceededing acount value"
-        return self.log(fail_msg)
+        return (self.account_value, self.log(fail_msg))
 
 
 def main():
     bank = Bank("BigMoneyStacksTrust")
     client = Client("Gunnar Gunnarson", 2137)
-    bank.register_client(client)
-    logged = client.withdraw(3)
-    print(logged)
+    msg = bank.register_client(client)
+    print(msg)
+    msg = client.withdraw(3)
+    print(msg)
     client2 = Client("John Johnson", 2137)
     msg = client.transfer(client2, 3)
     print(msg)
